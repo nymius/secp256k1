fbcd4fe43373bf07adbb77ef6f9d0bade8ac36ca
nymius <155548262+nymius@users.noreply.github.com>
2024-12-12 16:48:11 -0300

    tests: fix includes

--- src/modules/silentpayments/tests_impl.h
+++ src/modules/silentpayments/tests_impl.h
@@ -6,6 +6,7 @@
 #ifndef SECP256K1_MODULE_SILENTPAYMENTS_TESTS_H
 #define SECP256K1_MODULE_SILENTPAYMENTS_TESTS_H
 
+#include "../../../include/secp256k1.h"
 #include "../../../include/secp256k1_silentpayments.h"
 #include "../../../src/modules/silentpayments/vectors.h"
 
